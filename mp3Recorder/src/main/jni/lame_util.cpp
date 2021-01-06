7a321e6a2cbbcdc57263b6b86d4493a56506c457
dennishuang <iamdennisme@gmail.com>
2021-01-06 17:48:23 +0800

    feat:try native throw 异常（无法try到信号量错误，聊胜于无吧）

--- mp3Recorder/src/main/jni/lame_util.cpp
+++ mp3Recorder/src/main/jni/lame_util.cpp
@@ -10,58 +10,6 @@
 
 static lame_global_flags *lame = NULL;
 
-JNIEXPORT void JNICALL Java_com_czt_mp3recorder_util_LameUtil_init(
-	JNIEnv *env, jclass cls, jint inSamplerate, jint inChannel, jint outSamplerate, jint outBitrate, jint quality) {
-			if (lame != NULL) {
-        		lame_close(lame);
-        		lame = NULL;
-        	}
-        	lame = lame_init();
-        	lame_set_in_samplerate(lame, inSamplerate);
-        	lame_set_num_channels(lame, inChannel);//输入流的声道
-        	lame_set_out_samplerate(lame, outSamplerate);
-        	lame_set_brate(lame, outBitrate);
-        	lame_set_quality(lame, quality);
-        	lame_init_params(lame);
-}
-
-
-JNIEXPORT jint JNICALL Java_com_czt_mp3recorder_util_LameUtil_encode(
-		JNIEnv *env, jclass cls, jshortArray buffer_l, jshortArray buffer_r,
-		jint samples, jbyteArray mp3buf) {
-	jshort* j_buffer_l = env->GetShortArrayElements(buffer_l, NULL);
-
-	jshort* j_buffer_r = env->GetShortArrayElements(buffer_r, NULL);
-
-	const jsize mp3buf_size = env->GetArrayLength( mp3buf);
-
-	jbyte* j_mp3buf = env->GetByteArrayElements(mp3buf, NULL);
-
-    unsigned char* j_mp3buf_cpp = reinterpret_cast<unsigned char*>(j_mp3buf);
-
-	int result = lame_encode_buffer(lame, j_buffer_l, j_buffer_r,samples, j_mp3buf_cpp, mp3buf_size);
-
-	env->ReleaseShortArrayElements( buffer_l, j_buffer_l, 0);
-	env->ReleaseShortArrayElements( buffer_r, j_buffer_r, 0);
-	env->ReleaseByteArrayElements( mp3buf, j_mp3buf, 0);
-
-	return result;
-}
-
-JNIEXPORT jint JNICALL Java_com_czt_mp3recorder_util_LameUtil_flush(
-		JNIEnv *env, jclass cls, jbyteArray mp3buf) {
-	const jsize mp3buf_size = env->GetArrayLength(mp3buf);
-
-	jbyte* j_mp3buf = env->GetByteArrayElements( mp3buf, NULL);
-
-    unsigned char* j_mp3buf_cpp = reinterpret_cast<unsigned char*>(j_mp3buf);
-
-	int result = lame_encode_flush(lame, j_mp3buf_cpp, mp3buf_size);
-
-	env->ReleaseByteArrayElements(mp3buf, j_mp3buf, 0);
-
-	return result;
-}
 
 /**
  * jstring to char
@@ -85,8 +33,91 @@ char* Jstring2CStr(JNIEnv* env, jstring jstr) {
     return rtn;
 }
 
+void realInit(
+	JNIEnv *env, jclass cls, jint inSamplerate, jint inChannel, jint outSamplerate, jint outBitrate, jint quality) {
+			if (lame != NULL) {
+        		lame_close(lame);
+        		lame = NULL;
+        	}
+        	lame = lame_init();
+        	lame_set_in_samplerate(lame, inSamplerate);
+        	lame_set_num_channels(lame, inChannel);//输入流的声道
+        	lame_set_out_samplerate(lame, outSamplerate);
+        	lame_set_brate(lame, outBitrate);
+        	lame_set_quality(lame, quality);
+        	lame_init_params(lame);
+}
 
-JNIEXPORT jboolean JNICALL Java_com_czt_mp3recorder_util_LameUtil_wav2Mp3
+
+JNIEXPORT void JNICALL Java_com_czt_mp3recorder_util_LameUtil_init(
+	JNIEnv *env, jclass cls, jint inSamplerate, jint inChannel, jint outSamplerate, jint outBitrate, jint quality) {
+		try{
+		  realInit(env,cls,inSamplerate,inChannel,outSamplerate,outBitrate,quality);
+		}catch(...){
+		   env->ThrowNew(env->FindClass("java/lang/RuntimeException"),"init 捕获到lame throw native 异常");
+		}
+}
+
+jint realEncode(
+		JNIEnv *env, jclass cls, jshortArray buffer_l, jshortArray buffer_r,
+		jint samples, jbyteArray mp3buf) {
+	jshort* j_buffer_l = env->GetShortArrayElements(buffer_l, NULL);
+
+	jshort* j_buffer_r = env->GetShortArrayElements(buffer_r, NULL);
+
+	const jsize mp3buf_size = env->GetArrayLength( mp3buf);
+
+	jbyte* j_mp3buf = env->GetByteArrayElements(mp3buf, NULL);
+
+    unsigned char* j_mp3buf_cpp = reinterpret_cast<unsigned char*>(j_mp3buf);
+
+	int result = lame_encode_buffer(lame, j_buffer_l, j_buffer_r,samples, j_mp3buf_cpp, mp3buf_size);
+
+	env->ReleaseShortArrayElements( buffer_l, j_buffer_l, 0);
+	env->ReleaseShortArrayElements( buffer_r, j_buffer_r, 0);
+	env->ReleaseByteArrayElements( mp3buf, j_mp3buf, 0);
+
+	return result;
+}
+
+
+JNIEXPORT jint JNICALL Java_com_czt_mp3recorder_util_LameUtil_encode(
+		JNIEnv *env, jclass cls, jshortArray buffer_l, jshortArray buffer_r,
+		jint samples, jbyteArray mp3buf) {
+	        try{
+    		 return realEncode(env,cls,buffer_l,buffer_r,samples,mp3buf);
+    		}catch(...){
+    		   env->ThrowNew(env->FindClass("java/lang/RuntimeException"),"encode 捕获到lame throw native 异常");
+    		   return 0;
+    		}
+}
+
+jint realFlush(
+		JNIEnv *env, jclass cls, jbyteArray mp3buf) {
+	const jsize mp3buf_size = env->GetArrayLength(mp3buf);
+
+	jbyte* j_mp3buf = env->GetByteArrayElements( mp3buf, NULL);
+
+    unsigned char* j_mp3buf_cpp = reinterpret_cast<unsigned char*>(j_mp3buf);
+
+	int result = lame_encode_flush(lame, j_mp3buf_cpp, mp3buf_size);
+
+	env->ReleaseByteArrayElements(mp3buf, j_mp3buf, 0);
+
+	return result;
+}
+
+JNIEXPORT jint JNICALL Java_com_czt_mp3recorder_util_LameUtil_flush(
+		JNIEnv *env, jclass cls, jbyteArray mp3buf) {
+	      try{
+            return realFlush(env,cls,mp3buf);
+            }catch(...){
+              env->ThrowNew(env->FindClass("java/lang/RuntimeException"),"flush 捕获到lame throw native 异常");
+              return 0;
+  		}
+}
+
+jboolean realWav2Mp3
 (JNIEnv * env, jobject obj, jstring inputPath, jstring outputPath, jint inSamplerate, jint inChannel, jint outBitrate) {
     //初始化lame
     lame_t lame =  lame_init();
@@ -130,14 +161,32 @@ JNIEXPORT jboolean JNICALL Java_com_czt_mp3recorder_util_LameUtil_wav2Mp3
     return JNI_TRUE;
 }
 
-JNIEXPORT void JNICALL Java_com_czt_mp3recorder_util_LameUtil_close
+JNIEXPORT jboolean JNICALL Java_com_czt_mp3recorder_util_LameUtil_wav2Mp3
+(JNIEnv * env, jobject obj, jstring inputPath, jstring outputPath, jint inSamplerate, jint inChannel, jint outBitrate) {
+    try{
+       return realWav2Mp3(env,obj,inputPath,outputPath,inSamplerate,inChannel,outBitrate);
+    }catch(...){
+       env->ThrowNew(env->FindClass("java/lang/RuntimeException"),"wav2Mp3 捕获到lame throw native 异常");
+       return JNI_FALSE;
+    }
+    return JNI_TRUE;
+}
+
+
+void realClose
 (JNIEnv *env, jclass cls) {
 	lame_close(lame);
 	lame = NULL;
 }
 
-inline int fromJByte(jbyte pixel) {
-    // cast to unsigned char re-interprets values as 0-255
-    // cast to int will make intermediate calculations safer
-    return static_cast<int>(static_cast<unsigned char>(pixel));
+JNIEXPORT void JNICALL Java_com_czt_mp3recorder_util_LameUtil_close
+(JNIEnv *env, jclass cls) {
+	try{
+	   realClose(env,cls);
+	}catch(...){
+	   env->ThrowNew(env->FindClass("java/lang/RuntimeException"),"close 捕获到lame throw native 异常");
+	}
 }
+
+
+
